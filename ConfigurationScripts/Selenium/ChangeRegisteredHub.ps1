515164615fd03a3babd7cf1b2aca83bca0dfcfe9
Leeor.V <leeor.v@qualisystems.com>
2017-05-22 14:28:16 +0300

    Fixed service restart on config script

--- ConfigurationScripts/Selenium/ChangeRegisteredHub.ps1
+++ ConfigurationScripts/Selenium/ChangeRegisteredHub.ps1
@@ -1,4 +1,3 @@
 # The following command calls nssm to change the argument of the SeleniumGridNode service. '' is used to mark the end of the variable name
 nssm set SeleniumGridNode AppParameters -jar C:\selenium\selenium-server-standalone-3.4.0.jar -role node -hub http://$env:hub_server_address'':4444/grid/register
-sc stop SeleniumGridNode
-sc start SeleniumGridNode
+Restart-Service SeleniumGridNode
