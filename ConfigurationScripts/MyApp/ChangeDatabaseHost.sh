9e44cd2439d8700d7e0d082c3ffd58c15da80192
Leeor.V <leeor.v@qualisystems.com>
2017-05-25 15:44:30 +0300

    stabilization of config script

--- ConfigurationScripts/MyApp/ChangeDatabaseHost.sh
+++ ConfigurationScripts/MyApp/ChangeDatabaseHost.sh
@@ -2,4 +2,5 @@ sudo sed -i -e "s/database_host: 127.0.0.1/database_host: $database_server_addre
 ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 sudo pkill php
 sleep 5s
-sudo php /acme/bin/console server:start $ip:8000 --force & 
+sudo nohup php /acme/bin/console server:start $ip:8000 --force &>/dev/null &
+sudo disown
