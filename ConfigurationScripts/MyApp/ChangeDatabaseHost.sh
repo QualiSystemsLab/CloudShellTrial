59c81750f36c91e79de96bdf4fc0d364afbf10b6
Leeor.V <leeor.v@qualisystems.com>
2017-05-25 12:48:02 +0300

    Added & to prevent hanging during configuraiton

--- ConfigurationScripts/MyApp/ChangeDatabaseHost.sh
+++ ConfigurationScripts/MyApp/ChangeDatabaseHost.sh
@@ -1,5 +1,8 @@
 sudo sed -i -e "s/database_host: 127.0.0.1/database_host: $database_server_address/g" /acme/app/config/parameters.yml
 ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
-sudo php /acme/bin/console server:stop $ip:8000
 sleep 5s
-sudo php /acme/bin/console server:start $ip:8000
+sudo php /acme/bin/console server:stop $ip:8000 &
+
+sleep 5s
+sudo php /acme/bin/console server:start $ip:8000 &
+
