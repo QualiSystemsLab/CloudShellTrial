3344e38f0c71232896b95c192d4feac469364fd8
Leeor.V <leeor.v@qualisystems.com>
2017-06-18 17:22:26 +0300

    My App web config script - added color input

--- ConfigurationScripts/MyApp/ChangeDatabaseHost.sh
+++ ConfigurationScripts/MyApp/ChangeDatabaseHost.sh
@@ -1,1 +1,2 @@
-sudo sed -i -e "s/database_host: 127.0.0.1/database_host: $database_server_address/g" /acme/app/config/parameters.yml
+sudo sed -i -e "s/database_host: 127.0.0.1/database_host: $database_server_address/g" /acme/app/config/parameters.yml >null
+sudo sed -i -e "s/background-color: #2F21FF; }/background-color: #$app_color; }/g" /acme/web/assets/shop/css/style.css
