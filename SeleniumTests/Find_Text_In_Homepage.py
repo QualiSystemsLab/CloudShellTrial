276a3adebf97717f7ea75fef80923625443fd5aa
Leeor.V <leeor.v@qualisystems.com>
2017-06-19 17:48:40 +0300

    Fixing find text test

--- SeleniumTests/Find_Text_In_Homepage.py
+++ SeleniumTests/Find_Text_In_Homepage.py
@@ -13,9 +13,10 @@ print(datetime.strftime(datetime.now(), timeformat) + "Starting Selenium Web Dri
 selenium_driver = webdriver.Remote(command_executor="http://" + hub_address + ":4444/wd/hub", desired_capabilities={"browserName": "chrome"})
 print(datetime.strftime(datetime.now(), timeformat) + "Getting test page...")
 selenium_driver.get(test_url)
-print(datetime.strftime(datetime.now(), timeformat) + "Taking screenshot...")
+print(datetime.strftime(datetime.now(), timeformat) + "Looking for 'Latest products' text...")
 header = selenium_driver.find_element_by_css_selector(".ui.horizontal.section.divider.header")
-assert header
+assert "Latest products" in header.text
+print(datetime.strftime(datetime.now(), timeformat) + "Taking screenshot...")
 selenium_driver.get_screenshot_as_file('artifacts/Main_Page.png')
 selenium_driver.close()
 
