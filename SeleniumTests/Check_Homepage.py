c1e281792be1acb1753e5e229efd7d4da82a6b0f
Leeor.V <leeor.v@qualisystems.com>
2017-06-19 17:46:18 +0300

    Added tests to SeleniumTests Folder

--- SeleniumTests/Check_Homepage.py
+++ SeleniumTests/Check_Homepage.py
@@ -9,11 +9,11 @@ argsdict = {argument.split("=")[0]: argument.split("=")[1] for argument in sys.a
 test_url = "http://" + argsdict["target"]
 hub_address = argsdict["hub"]
 
-print(datetime.strftime(datetime.now(), timeformat) + "Starting Selenium Web Driver...\n")
+print(datetime.strftime(datetime.now(), timeformat) + "Starting Selenium Web Driver...")
 selenium_driver = webdriver.Remote(command_executor="http://" + hub_address + ":4444/wd/hub", desired_capabilities={"browserName": "chrome"})
-print(datetime.strftime(datetime.now(), timeformat) + "Getting test page...\n")
+print(datetime.strftime(datetime.now(), timeformat) + "Getting test page...")
 selenium_driver.get(test_url)
-print(datetime.strftime(datetime.now(), timeformat) + "Taking screenshot...\n")
+print(datetime.strftime(datetime.now(), timeformat) + "Taking screenshot...")
 selenium_driver.get_screenshot_as_file('artifacts/Main_Page.png')
 selenium_driver.close()
 
---
+++ SeleniumTests/Find_Text_In_Homepage.py
@@ -0,0 +1,22 @@
+# argdict example: {'target': ['10.0.0.74:8000'], 'hub': ['10.0.0.193']}
+
+import sys
+from datetime import datetime
+from selenium import webdriver
+timeformat = "[%m-%d_%H:%M:%S.%f] "
+argsdict = {argument.split("=")[0]: argument.split("=")[1] for argument in sys.argv if '=' in argument}
+
+test_url = "http://" + argsdict["target"]
+hub_address = argsdict["hub"]
+
+print(datetime.strftime(datetime.now(), timeformat) + "Starting Selenium Web Driver...")
+selenium_driver = webdriver.Remote(command_executor="http://" + hub_address + ":4444/wd/hub", desired_capabilities={"browserName": "chrome"})
+print(datetime.strftime(datetime.now(), timeformat) + "Getting test page...")
+selenium_driver.get(test_url)
+print(datetime.strftime(datetime.now(), timeformat) + "Taking screenshot...")
+header = selenium_driver.find_element_by_css_selector(".ui.horizontal.section.divider.header")
+assert header
+selenium_driver.get_screenshot_as_file('artifacts/Main_Page.png')
+selenium_driver.close()
+
+print(datetime.strftime(datetime.now(), timeformat) + "Test Completed Successfully")
