43b5dc9c93d5f572a54b4017d8615a86007173f2
Leeor.V <leeor.v@qualisystems.com>
2017-07-06 17:47:19 +0300

    Staging Health Check - added requirements, fixed incorrect references to elb instead of cf

--- BlueprintScripts/MyApp_Staging/Health Check/__main__.py
+++ BlueprintScripts/MyApp_Staging/Health Check/__main__.py
@@ -24,14 +24,14 @@ elif requests.get("http://" + elb_url).status_code != 200:
 	healthcheck_passed = False
 
 cf_service = next(service for service in reservation_details.ReservationDescription.Services if service.ServiceName == "AWS CloudFront Distribution")
-cf_url = next(attribute.Value for attribute in elb_service.Attributes if attribute.Name == "External_URL")
+cf_url = next(attribute.Value for attribute in cf_service.Attributes if attribute.Name == "External_URL")
 
 print("Attempting to reach CloudFront Distribution...")
 
 if not cf_url:
 	print("No CF Distribution is deployed!")
 	healthcheck_passed = False
-elif requests.get("http://" + elb_url).status_code != 200:
+elif requests.get("http://" + cf_url).status_code != 200:
 	print("CloudFront Distribution is unreachable!")
 	healthcheck_passed = False
 
