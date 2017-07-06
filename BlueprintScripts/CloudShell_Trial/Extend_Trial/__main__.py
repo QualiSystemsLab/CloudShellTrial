473cfd835f9568b71667d770a1affc90daa8329c
Leeor.V <leeor.v@qualisystems.com>
2017-07-06 17:19:06 +0300

    Extend Trial - added hubspot enrollment and end-date recalculation, preset to 5 days.

--- BlueprintScripts/CloudShell_Trial/Extend_Trial/__main__.py
+++ BlueprintScripts/CloudShell_Trial/Extend_Trial/__main__.py
@@ -16,11 +16,18 @@ company = global_inputs["Company Name"]
 phone = global_inputs["Phone number"]
 owner_email = global_inputs["Quali Owner"]
 
-extension_period_in_days = parameter["Extension Period"]
+extension_period_in_days = "5"
 api = CloudShellAPISession(host=connectivityContext["serverAddress"], token_id=connectivityContext["adminAuthToken"], domain=reservationContext["domain"])
 
 api.ExtendReservation(reservationContext["id"], 24 * 60 * int(extension_period_in_days))
-# TODO: Extend trial campaign in hubspot and send relevant e-mail
+reservation_details = api.GetReservationDetails(reservationContext["id"]).ReservationDescription
+reservation_end_date = datetime.datetime.strptime(reservation_details.EndTime, "%d/%m/%Y %H:%M")
+reservation_end_time_in_ms = int((res_end_date - datetime.datetime.utcfromtimestamp(0)).total_seconds() * 1000)
+
+hubspot_helper = Hubspot_API_Helper("cba66474-e4e4-4f5b-9b9b-35620577f343")
+hubspot_helper.change_contact_property(email, "cloudshell_trial_end_date", str(reservation_end_time_in_ms))
+hubspot_helper.enroll_contact_to_workflow(email, "2012345")
+
 # Todo send "Extended Trial" e-mail to owner + admin
 
 api.WriteMessageToReservationOutput(reservationContext["id"], "Trial Extended by " + extension_period_in_days + " days")
