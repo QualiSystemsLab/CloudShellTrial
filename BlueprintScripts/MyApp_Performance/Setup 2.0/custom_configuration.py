7e8c4c67c360d817f53b72691132b0022ceb8355
Leeor.V <leeor.v@qualisystems.com>
2017-07-10 18:04:12 +0300

    MyApp Performance setup: Added requests requirement, fixed availability healthcheck

--- BlueprintScripts/MyApp_Performance/Setup 2.0/custom_configuration.py
+++ BlueprintScripts/MyApp_Performance/Setup 2.0/custom_configuration.py
@@ -34,10 +34,20 @@ def config_web_servers(sandbox, components):
 	my_app_addresses_string = ",".join(my_app_addresses)
 	sandbox.apps_configuration.set_config_param(ha_proxy, 'web_server_addresses', my_app_addresses_string)
 	sandbox.apps_configuration.apply_apps_configurations([ha_proxy])
+
 	ha_proxy_port = next(attribute.Value for attribute in ha_proxy.app_request.app_resource.LogicalResource.Attributes if attribute.Name == "WWW_Port")
 	wait_time = 0
-	while not (200 <= requests.get("http://" + ha_proxy.deployed_app.FullAddress + ":" + ha_proxy_port).status_code < 300):
-		sleep(5)
-		wait_time += 5
+	ha_proxy_details = api.GetResourceDetails(ha_proxy.deployed_app.Name)
+	ha_proxy_public_ip = next(att.Value for att in ha_proxy_details.ResourceAttributes if att.Name == "Public IP")
+	wait_time = 0
+	my_app_available = False
+	while not my_app_available:
+		try:
+			get_result = requests.get("http://" + ha_proxy_public_ip + ":" + ha_proxy_port)
+			my_app_available = (200 <= get_result.status_code < 300)
+		except requests.ConnectionError:
+			sleep(5)
+			wait_time += 5
+
 		if wait_time > 300:
 			raise Exception("Timeout while waiting for My App availability")
