424242befd022b65841c20946d164f7f021b89f2
Leeor.V <leeor.v@qualisystems.com>
2017-07-10 12:25:23 +0300

    Added LB generated cookie support to HA Proxy configuration

--- ConfigurationScripts/HAProxy/AppendBackendServers.sh
+++ ConfigurationScripts/HAProxy/AppendBackendServers.sh
@@ -1,7 +1,7 @@
 index=1
 for web_server_address in $(echo $web_server_addresses | tr "," "\n")
 do
-  echo "	server my_app$index $web_server_address:8000 check" | sudo tee -a /etc/haproxy/haproxy.cfg
+  echo "    server my_app$index $web_server_address:8000 check cookie my_app$index" | sudo tee -a /etc/haproxy/haproxy.cfg
   index=$(($index+1))
 done
 sudo service haproxy reload
