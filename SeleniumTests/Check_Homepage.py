355a198fe29824d8f133e46423777b29dcfbc0ea
Leeor.V <leeor.v@qualisystems.com>
2017-08-22 12:07:05 +0300

    Fix field feedback - test passes when my app website is down

--- SeleniumTests/Check_Homepage.py
+++ SeleniumTests/Check_Homepage.py
@@ -14,6 +14,9 @@ print(datetime.strftime(datetime.now(), timeformat) + "Starting Selenium Web Dri
 selenium_driver = webdriver.Remote(command_executor="http://" + hub_address + ":4444/wd/hub", desired_capabilities={"browserName": "chrome"})
 print(datetime.strftime(datetime.now(), timeformat) + "Getting test page...")
 selenium_driver.get(test_url)
+if "ERR" in selenium_driver.find_element_by_id("main-frame-error").text:
+	raise RuntimeError("Test Failed: could not get Test Page")
+
 print(datetime.strftime(datetime.now(), timeformat) + "Taking screenshot...")
 selenium_driver.get_screenshot_as_file(artifacts_folder + "/Main_Page.png")
 selenium_driver.close()
