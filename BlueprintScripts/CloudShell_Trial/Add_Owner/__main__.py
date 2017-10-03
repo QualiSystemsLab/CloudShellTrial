155eac9cd9df6e116a8c09c9f7c5658db4ae3c0f
Leeor.V <leeor.v@qualisystems.com>
2017-10-03 10:11:17 +0300

    CloudShell Trial Automation: Added "Add Owner" command

---
+++ BlueprintScripts/CloudShell_Trial/Add_Owner/__main__.py
@@ -0,0 +1,30 @@
+from cloudshell.api.cloudshell_api import *
+from email_helper import SMTPClient
+from os import environ as parameter
+import json
+import requests
+import datetime
+from hubspot_helper import Hubspot_API_Helper
+
+reservationContext = json.loads(parameter["reservationContext"])
+connectivityContext = json.loads(parameter["qualiConnectivityContext"])
+
+global_inputs = {in_param["parameterName"]:in_param["value"] for in_param in reservationContext["parameters"]["globalInputs"]}
+first_name = global_inputs["First Name"]
+last_name = global_inputs["Last Name"]
+email = global_inputs["email"]
+company = global_inputs["Company Name"]
+phone = global_inputs["Phone number"]
+owner_email = global_inputs["Quali Owner"]
+username = email
+
+new_owner = parameter["New_Owner"]
+
+api = CloudShellAPISession(host=connectivityContext["serverAddress"], token_id=connectivityContext["adminAuthToken"], domain=reservationContext["domain"])
+
+user_info = api.GetUserDetails(username)
+group_name = user_info.Groups[0].Name
+api.AddUsersToGroup([new_owner], group_name)
+
+api.WriteMessageToReservationOutput(reservationContext["id"], """{0} was successfully added as an owner to {1}'s CloudShell VE Trial, 
+please make sure to monitor any feedback or assistance that might be required from the Trial User""".format(new_owner, username))
