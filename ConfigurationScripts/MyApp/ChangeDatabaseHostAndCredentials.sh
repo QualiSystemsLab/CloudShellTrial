12b7cb0c99d52d41204d283e8f27779d5e2124f9
Leeor Vardi <leeor.v@qualisystems.com>
2019-06-18 07:59:22 +0300

    Fixed incorrect text replacement

--- ConfigurationScripts/MyApp/ChangeDatabaseHostAndCredentials.sh
+++ ConfigurationScripts/MyApp/ChangeDatabaseHostAndCredentials.sh
@@ -1,4 +1,4 @@
 sudo sed -i -e "s/database_host: 127.0.0.1/database_host: $database_server_address/g" /acme/app/config/parameters.yml >null
 sudo sed -i -e "s/background-color: #f9fAfb; }/background-color: #$app_color; }/g" /acme/web/assets/shop/css/style.css
-sudo sed -i -e "s/database_user: dbuser/database_host: $database_user/g" /acme/app/config/parameters.yml >null
+sudo sed -i -e "s/database_user: dbuser/database_user: $database_user/g" /acme/app/config/parameters.yml >null
 sudo sed -i -e "s/database_password: i-00265dc25461c4061; }/background-color: #$database_password; }/g" /acme/web/assets/shop/css/style.css
